c602eee62c8a0248ff2dbb67b44aeb586b3721fb
Kissami Youssef <kissamiyoussef70@gmail.com>
2026-04-29 23:57:13 +0100

    etape2: Dynamiser Controllers et Templates

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,16 +11,32 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CategoryController extends AbstractController
 {
     #[Route('/categories', name: 'app_categories')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('category/index.html.twig');
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
     }
 
     #[Route('/category/{id}', name: 'app_category_products')]
-    public function productsByCategory(int $id): Response
-    {
+    public function productsByCategory(
+        int $id,
+        CategoryRepository $categoryRepository,
+        ProductRepository $productRepository
+    ): Response {
+        $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Catégorie introuvable !');
+        }
+
+        $products = $productRepository->findBy(['category' => $category]);
+
         return $this->render('category/products.html.twig', [
-            'id' => $id,
+            'category' => $category,
+            'products' => $products,
         ]);
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductController extends AbstractController
 {
     #[Route('/product/{id}', name: 'app_product_details')]
-    public function details(int $id): Response
+    public function details(int $id, ProductRepository $productRepository): Response
     {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Produit introuvable !');
+        }
+
         return $this->render('product/index.html.twig', [
-            'id' => $id,
+            'product' => $product,
         ]);
     }
 }
